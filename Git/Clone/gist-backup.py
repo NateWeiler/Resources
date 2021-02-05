e3335ea657b2744f3ddf2d1ba2ee93edc1b78c88
NateWeiler <nateweiler84@gmail.com>
2021-02-05 03:45:20 -0500

    Add

--- Git/Clone/gist-backup.py
+++ Git/Clone/gist-backup.py
@@ -1,24 +1,60 @@
-#!/usr/bin/env python3
-# Clone or update all a user's gists
-# curl -ks https://gist.githubusercontent.com/nicerobot/1622504/raw/backup.sh | bash
-# curl -ks https://gist.githubusercontent.com/nicerobot/1622504/raw/backup.sh | USER=nicerobot bash
+#! /usr/bin/env python
 
+# Clone or update all the GitHub gists of the current GitHub user into working directory
+# Specify GitHub username in the GITHUB_USER environment variable, i.e.
+# git config --global user.name "NateWeiler"
+
+# To execute, copy script into a ~/gists directory, and 'python gist-backup.py'
+# This will git clone every public and private gist of GITHUB-USER into that folder
+# as well as a contents.txt folder.
+
+from __future__ import print_function
 import json
+import urllib
 from subprocess import call
-from urllib.request import urlopen
 import os
-USER = os.environ['USER']
+import math
 
-with urlopen('https://api.github.com/users/'+USER+'/gists') as u:
-  startd = os.getcwd()
+try:
+    from urllib.request import urlopen
+except ImportError:
+    from urllib2 import urlopen
 
-  for gist in json.loads(u.read().decode('utf8')):
-    gistd = os.path.basename(gist['html_url'])
-    pull = gist['git_pull_url']
-    print(pull)
-    if os.path.isdir(gistd):
-      os.chdir(gistd)
-      call(['git', 'pull', pull])
-      os.chdir(startd)
-    else:
-      call(['git', 'clone', pull])
+if 'GITHUB_USER' in os.environ:
+    USER = os.environ['GITHUB_USER']
+else:
+    USER = os.environ['USER']
+
+perpage = 30
+userurl = urlopen('https://api.github.com/users/' + USER)
+public_gists = json.load(userurl)
+gistcount = public_gists['public_gists']
+print("Found gists: " + str(gistcount))
+pages = int(math.ceil(float(gistcount) / perpage))
+print("Found pages: " + str(pages))
+
+f = open('./contents.txt', 'w+')
+
+for page in range(pages):
+    pageNumber = str(page + 1)
+    print("Processing page number " + pageNumber)
+    pageUrl = 'https://api.github.com/users/' + USER  + '/gists?page=' + \
+        pageNumber + '&per_page=' + str(int(perpage))
+    u = urlopen (pageUrl)
+    gists = json.load(u)
+    startd = os.getcwd()
+    for gist in gists:
+        gistd = gist['id']
+        gistUrl = 'https://gist.github.com/' + gistd + '.git'
+        if os.path.isdir(gistd):
+            os.chdir(gistd)
+            call(['git', 'pull', gistUrl])
+            os.chdir(startd)
+        else:
+            call(['git', 'clone', gistUrl])
+        if gist['description'] == None:
+            description = ''
+        else:
+            description = gist['description'].encode('utf8').\
+                replace("\r", ' ').replace("\n", ' ')
+        print(gist['id'], gistUrl, description, file=f)
--- Git/backup.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-curl -ks https://gist.githubusercontent.com/nicerobot/1622504/raw/gist-backup.py | USER=${USER} python3
